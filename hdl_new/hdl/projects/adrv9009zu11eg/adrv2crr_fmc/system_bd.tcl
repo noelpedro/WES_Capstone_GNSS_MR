1ade166c59d213f28f7b4598d433cd7c4d61b06f
badger <npm@ucsd.edu>
2022-05-28 16:13:58 +0000

    Deleting hdl source code

--- hdl_new/hdl/projects/adrv9009zu11eg/adrv2crr_fmc/system_bd.tcl
+++
@@ -1,9 +0,0 @@
-
-source ../common/adrv9009zu11eg_bd.tcl
-source ../common/adrv2crr_fmc_bd.tcl
-
-ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
-ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init_sys.txt"
-ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
-
-sysid_gen_sys_init_file
